83d77a9de19c83683d700ad87cf53ffb8ecaa7b0
JustVais <justvais@mail.ru>
2020-05-01 16:45:23 +0500

    decoded elements are marked with the - decoded class

--- script.js
+++ script.js
@@ -3,22 +3,26 @@ console.clear();
 function checkingForNewElements() {
     new Promise(() => {
         let oldMesCount = 0;
-    
+
         setInterval(() => {
             let elements = document.getElementsByClassName("im-mess--text");
-    
+
             if (elements.length > oldMesCount) {
                 oldMesCount = elements.length;
                 decodeMessages(elements);
             }
-        }, 5000);
+        }, 2000);
     });
 }
 
 
 let decodeMessages = (elements) => {
     for (let i = 0; i < elements.length; i++) {
-        elements[i].innerHTML = "gg";
+        if (!elements[i].classList.contains('decoded')) {
+            elements[i].innerHTML = "true";
+        }
+
+        elements[i].classList.add("decoded");
     }
 }
 
